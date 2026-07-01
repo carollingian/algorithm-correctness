02a9c79ea31643066dbd6e6426340b1ceb11e84b
carollingian <carolina.fdcampos@gmail.com>
2026-07-01 00:06:43 -0300

    feat: basic doc structure

--- src/ord_equiv.v
+++ src/ord_equiv.v
@@ -4,16 +4,23 @@ From Stdlib Require Import Lia.
 From Stdlib Require Import List.
 (* end hide *)
 
-(** Equivalência entre diferentes noções de ordenação.*)
+(** * Introdução *)
 
-(**
+(** Equivalência entre diferentes noções de ordenação. *)
+(** O objetivo deste arquivo é formalizar e demonstrar a equivalência 
+    entre quatro definições distintas de listas ordenadas. *)
 
-A primeira definição de ordenação, chamada [ord1], é uma definição indutiva contendo 3 regras de formação:
+(** * Desenvolvimento *)
 
+(** ** Definições de ordenação e propriedades básicas *)
+
+(** *** Ordenação 1 (ord1) *)
+
+(** A primeira definição de ordenação, chamada [ord1], é uma definição 
+    indutiva contendo 3 regras de formação:
 %\begin{mathpar}
  \inferrule*[Right={$(ord1\_nil)$}]{~}{ord1\ nil} \and \inferrule*[Right={$(ord1\_one)$}]{~}{ord1\ (x::nil)} \and \inferrule*[Right={$(ord1\_all)$}]{x \leq y \and ord1(y::l)}{ord1\ (x::y::l)}
 \end{mathpar}%
-
 *)
 
 Inductive ord1 : list nat -> Prop :=
@@ -21,32 +28,35 @@ Inductive ord1 : list nat -> Prop :=
 | ord1_one: forall x, ord1 (x::nil)
 | ord1_all: forall l x y, x <= y -> ord1 (y::l) -> ord1 (x::y::l).
 
-(**
+(** *** Ordenação 2 (ord2) *)
 
-A segunda definição de ordenação, chamada [ord2], é uma definição indutiva contendo 2 regras de formação:
-
+(** A segunda definição de ordenação, chamada [ord2], é uma definição 
+    indutiva contendo 2 regras de formação:
 %\begin{mathpar}
  \inferrule*[Right={$(ord2\_nil)$}]{~}{ord2\ nil} \and  \inferrule*[Right={$(ord2\_all)$}]{x \leq^* l \and ord2\ l}{ord2\ (x::l)}
 \end{mathpar}%
-%\noindent% onde $x \leq^* l$ significa que $x$ é menor ou igual que todo elemento da lista $l$. Formalmente, este predicado é definido como a seguir:
-*)
+%\noindent% onde $x \leq^* l$ significa que $x$ é menor ou igual que 
+todo elemento da lista $l$. Formalmente, este predicado é definido a seguir: *)
 
 Definition le_all x l := forall y, In y l -> x <= y.
+
 (* begin hide *)
 Notation "x <=* l" := (le_all x l) (at level 60).  
 (* end hide *)
+
 (** printing <=* %\ensuremath{\leq^*}% *)
 Inductive ord2 : list nat -> Prop :=
 | ord2_nil: ord2 nil
 | ord2_all: forall l x, x <=* l -> ord2 l -> ord2 (x::l).
 (** printing < %\ensuremath{<}% *)
-(**
 
-A terceira definição de ordenação, chamada [ord3], diz que uma lista está ordenada se cada elemento é menor ou igual ao elemento seguinte:
+(** *** Ordenação 3 (ord3) *)
 
- *)
+(** A terceira definição de ordenação, chamada [ord3], diz que uma lista 
+    está ordenada se cada elemento é menor ou igual ao elemento seguinte: *)
 
-Definition ord3 (l : list nat) : Prop := forall i, length l > 1 -> (S i) < length l -> nth i l 0 <= nth (S i) l 0.
+Definition ord3 (l : list nat) : Prop := 
+  forall i, length l > 1 -> (S i) < length l -> nth i l 0 <= nth (S i) l 0.
 
 Lemma ord3_nil: ord3 nil.
 Proof.
@@ -65,19 +75,27 @@ Proof.
   - lia.
 Qed.
 
+(** *** Ordenação 4 (ord4) *)
 
-(**
+(** A quarta definição de ordenação, chamada [ord4], diz que uma lista 
+    está ordenada se cada elemento é menor ou igual que qualquer elemento 
+    que esteja em posição anterior: *)
 
-A quarta definição de ordenação, chamada [ord4], diz que uma lista está ordenada se cada elemento é menor ou igual que qualquer elemento que esteja em posição anterior:
-
- *)
-
-Definition ord4 (l : list nat) : Prop := forall i j, i < j -> j < length l -> nth i l 0 <= nth j l 0.
+Definition ord4 (l : list nat) : Prop := 
+  forall i j, i < j -> j < length l -> nth i l 0 <= nth j l 0.
 
 Lemma ord4_nil: ord4 nil.
 Proof. Admitted.
 
-(** O objetivo desta proposta é mostrar que as definições [ord1], [ord2], [ord3] e [ord4] são equivalentes: *)
+(** ** Lemas auxiliares de equivalência *)
+
+(** Colocar aqui as provas direcionais que servem de base para os 
+   teoremas principais. *)
+
+(** ** Teoremas principais *)
+
+(** O objetivo principal desta proposta é mostrar que as definições 
+    [ord1], [ord2], [ord3] e [ord4] são logicamente equivalentes: *)
 
 Theorem ord1_equiv_ord2: forall l, ord1 l <-> ord2 l.
 Proof.
@@ -104,3 +122,6 @@ Proof.
 Admitted.
 
 
+(** * Conclusão *)
+
+(** Concluir aqui. *)
